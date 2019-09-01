53d6c9b1201ef2d3c722bdfd146f0c82e29780e1
ASA1976 <rap.paradigm@gmail.com>
2019-09-01 04:14:39 -0400

    Call Any Stored Procedure
    - Alternative to std::function
    - Intended for application programming (not for systems programming)
    - Safe while delivering excellent run-time performance

---
+++ example.cpp
@@ -0,0 +1,43 @@
+#include "function"
+#include <cstdio>
+
+using namespace function;
+using Invocative = const Procedural< void, const char* >;
+
+const struct Class {
+
+    void operator()( const char* message ) const 
+    {
+        puts( message );
+    }
+
+    void member( const char* message ) const 
+    {
+        puts( message );
+    }
+
+} Object;
+
+auto Lambda = []( const char* message ) 
+{
+    puts( message );
+};
+
+void Function( const char* message ) 
+{
+    puts( message );
+}
+
+void Demonstrate( Invocative& invoke, const char* message ) 
+{
+    invoke( message );
+}
+
+int main() 
+{
+    constexpr auto& Type = Deduction< void, const char* >;
+    Demonstrate( Designate( Object, Type ), "Functor" );
+    Demonstrate( Designate( Object, &Class::member, Type ), "Member Function" );
+    Demonstrate( Designate( Lambda, Type ), "Lambda" );
+    Demonstrate( Designate( Function ), "Function" );
+}
