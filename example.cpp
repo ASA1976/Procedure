dac691e27128ec4fbb15cc830b02a70004adef23
ASA1976 <rap.paradigm@gmail.com>
2019-09-04 13:12:27 -0400

    Call Any Stored Procedure

--- example.cpp
+++ example.cpp
@@ -2,32 +2,34 @@
 // Licensed under the Academic Free License version 3.0
 #define FUNCTION_HEADER "function" // <function>
 #include FUNCTION_HEADER
-#include <cstdio>
+#include <iostream>
+#include <string>
 
+using namespace std;
 using namespace function;
-using Demonstrative = const Procedural< void, const char* >;
+using Demonstrative = const Procedural< void, const string& >;
 
 const struct Class {
 
-    void operator()( const char* message ) const {puts( message );}
+    void operator()( const string& message ) const {cout << message << endl;}
 
-    void member( const char* message ) const {puts( message );}
+    void member( const string& message ) const {cout << message << endl;}
 
 } Object;
 
-auto Lambda = []( const char* message ) {puts( message );};
+auto Lambda = []( const string& message ) {cout << message << endl;};
 
-void Function( const char* message ) {puts( message );}
+void Function( const string& message ) {cout << message << endl;}
 
-void Demonstrate( Demonstrative& invoke, const char* message ) 
+void Demonstrate( Demonstrative& invoke, const string& message ) 
 { 
     invoke( message ); 
 }
 
 int main() 
 {
-    static const auto& Type = Deduction< void, const char* >;
-    static const auto& Member = &Class::member;
+    static const auto& Type = Deduction< void, const string& >;
+    static const auto& Member = &Class::member; // &decltype(Object)::member
     Demonstrate( Designate( Object, Type ), "Functor" );
     Demonstrate( Designate( Object, Member, Type ), "Member Function" );
     Demonstrate( Designate( Lambda, Type ), "Lambda" );
